dddf9f88efe63e0d0416a2b0bb97a932f4e53255
铭zy <yinty86@163.com>
2025-08-09 00:38:48 +0800

    Update install_Kirara_AI_bot.sh

--- install_Kirara_AI_bot.sh
+++ install_Kirara_AI_bot.sh
@@ -1,7 +1,8 @@
-echo "更新软件源："
-apt-get update -y
-echo "安装docker/docker compose："
-bash <(curl -sSL https://linuxmirrors.cn/docker.sh)
-
-
-docker-compose up -d 
+回声"更新软件源："
+apt-get更新-y
+回声"安装docker/docker组成："
+bash<(卷曲-sSL https://linuxmirrors.cn/docker.sh)
+回声"拉取安装索引"
+wget https://github.com/mingzy-lz/lzp_install/blob/main/docker-compose.yml
+回声"安装"
+docker-compose up-d
