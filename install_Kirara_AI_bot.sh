520328b501ae6bc0a2c59c208a1beb39e079077b
铭zy <yinty86@163.com>
2025-08-11 22:08:01 +0800

    Delete install_Kirara_AI_bot.sh

--- install_Kirara_AI_bot.sh
+++
@@ -1,8 +0,0 @@
-回声"更新软件源："
-apt-get更新-y
-回声"安装docker/docker组成："
-bash<(卷曲-sSL https://linuxmirrors.cn/docker.sh)
-回声"拉取安装索引"
-wget https://github.com/mingzy-lz/lzp_install/blob/main/docker-compose.yml
-回声"安装"
-docker-compose up-d
