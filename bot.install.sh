96b39986e8ff84138f0b1554d2252532105903ba
铭zy <yinty86@163.com>
2025-08-11 21:46:14 +0800

    Update bot.install.sh

--- bot.install.sh
+++ bot.install.sh
@@ -1,23 +1,25 @@
-# 换源：
+#!/bin/bash
+set -e  # 遇到错误立即退出
+
+# 换源
+echo "===== 开始更换系统源 ====="
 bash <(curl -sSL https://linuxmirrors.cn/main.sh)
-# 安装docker：
+
+# 安装Docker
+echo -e "\n===== 开始安装Docker ====="
 bash <(curl -sSL https://linuxmirrors.cn/docker.sh)
 
-# 创建astrbot文件夹：
-mkdir astrbot
-# 进入astrbot文件夹：
-cd astrbot
-# 下载bot安装索引：
-wget https://raw.githubusercontent.com/NapNeko/NapCat-Docker/main/compose/astrbot.yml
-# 安装bot
+# 部署astrbot
+echo -e "\n===== 开始部署astrbot ====="
+mkdir -p astrbot && cd astrbot  # 创建并进入文件夹（-p确保父目录存在）
+wget -O astrbot.yml https://raw.githubusercontent.com/NapNeko/NapCat-Docker/main/compose/astrbot.yml
 sudo docker compose -f astrbot.yml up -d
 
-# 再次启动：
-sudo docker compose -f astrbot.yml up -d
-
-ehon 
-"浏览器打开：你的服务器IP:6099
-登录界面输入：napcat（默认）
-同上：
-服务器IP:6185
-astrbot（默认）"
+# 显示访问信息
+echo -e "\n===== 部署完成！访问信息如下 ====="
+echo "1. Napcat 访问：浏览器打开 http://你的服务器IP:6099"
+echo "   默认登录名：napcat"
+echo "2. Astrbot 访问：浏览器打开 http://你的服务器IP:6185"
+echo "   默认登录名：astrbot"
+echo -e "\n提示：若需重启服务，可执行以下命令："
+echo "cd astrbot && sudo docker compose -f astrbot.yml up -d"
